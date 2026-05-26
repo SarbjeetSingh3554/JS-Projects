d1629f3378fcf239c2fa4df4737fa821651b0245
Sarbjeet Singh <sarbjeetsingh9416@gmail.com>
2026-05-27 01:09:13 +0530

    project6

---
+++ project6/script.js
@@ -0,0 +1,18 @@
+const insert=document.getElementById('insert')
+
+window.addEventListener('keydown',(e)=>{
+    insert.innerHTML=`
+    <div class='color'>
+        <table>
+            <tr>
+                <td>Key Pressed</td>
+                <td>Key code</td>
+            </tr>
+            <tr>
+                <td>${e.key===' '?'Space':e.key}</td>
+                <td>${e.code}</td>
+            </tr>
+        </table>
+    </div>
+    `
+})
